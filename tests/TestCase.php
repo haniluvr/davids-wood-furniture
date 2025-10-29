ca2391ca8d5e8a838b6758124acb8fc595aec9ad
Hana <hvniluvr@gmail.com>
2025-10-29 11:54:09 +0800

    Version 1.8.13

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -8,4 +8,13 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 abstract class TestCase extends BaseTestCase
 {
     use RefreshDatabase;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        // Ensure we're using the test database
+        config(['database.default' => 'mysql']);
+        config(['database.connections.mysql.database' => 'davids_wood_test']);
+    }
 }
